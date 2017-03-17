abc3650015925438eaf2d9e74d702a0d2d31ce90
rtrang <roger.trang@gmail.com>
2017-03-17 13:45:00 -0700

    Update lList.java

--- lList.java
+++ lList.java
@@ -3,7 +3,7 @@ import java.util.*;
 // reserse words using linked list
 public class lList{
 	public static void main(String[] args){
-	String[] a = {"this","is","test"};
+	String[] a = {"this","is","a","test"};
 	LinkedList<String> list = new LinkedList<String>();
 	for(int i=0; i<=a.length-1; i++){
 	list.add(a[i]);
